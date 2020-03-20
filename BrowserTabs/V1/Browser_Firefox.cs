360e958e2e4f78cfb6ce5ffc3f13794c90b49e7e
Diego Leonardo Arias Mora <dilearmo@hotmail.com>
2020-03-20 17:03:26 -0600

    Fix for Firefox

--- BrowserTabs/V1/Browser_Firefox.cs
+++ BrowserTabs/V1/Browser_Firefox.cs
@@ -29,61 +29,46 @@ namespace V1.BrowserTabs
                 AutomationElement rootElement = AutomationElement.FromHandle(process.MainWindowHandle);
 
                 Condition condDocAll = new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Document);
-                AutomationElementCollection elements = rootElement.FindAll(TreeScope.Descendants, condDocAll);
+                AutomationElementCollection tabElements = rootElement.FindAll(TreeScope.Descendants, condDocAll);
+                List<BrowserTab> tabs = ListTabs(tabElements, rootElement.Current.Name);
 
-                foreach (AutomationElement docElement in elements)
+                BrowserWindow window = new BrowserWindow
                 {
-                    List<BrowserTab> tabs = ListTabs(docElement);
-                    BrowserWindow window = new BrowserWindow
-                    {
-                        CurrentTab = tabs?.Where((x) => x.IsCurrentTab)?.FirstOrDefault(),
-                        Tabs = tabs,
-                        IsMinimized = tabs.Count == 0 // falta setearlo bien
-                    };
+                    CurrentTab = tabs?.Where((x) => x.IsCurrentTab)?.FirstOrDefault(),
+                    Tabs = tabs,
+                    IsMinimized = tabs.Count == 0 // falta setearlo bien
+                };
 
-                    result.Add(window);
-                }
+                result.Add(window);
             }
             return result;
         }
 
-        private List<BrowserTab> ListTabs(AutomationElement docElement)
+        private List<BrowserTab> ListTabs(AutomationElementCollection tabElements, string windowName)
         {
             List<BrowserTab> result = new List<BrowserTab>();
+            foreach (AutomationElement tab in tabElements)
+            {
+                result.Add(new BrowserTab
+                {
+                    Name = tab.Current.Name,
+                    Url = GetUrl(tab),
+                    IsCurrentTab = windowName.Contains(tab.Current.Name)
+                });
+            }
+            return result;
+        }
+
+        private string GetUrl(AutomationElement docElement)
+        {
             foreach (AutomationPattern pattern in docElement.GetSupportedPatterns())
             {
                 if (docElement.GetCurrentPattern(pattern) is ValuePattern)
                 {
-                    var elementName = (docElement.GetCurrentPattern(pattern) as ValuePattern).Current.Value.ToString();
-                    Console.WriteLine(elementName);
-                    string windowName = docElement.Current.Name;
-                    result.Add(new BrowserTab
-                    {
-                        Name = elementName,
-                        Url = windowName.Contains(elementName) ? FindWindowUrl(Process.GetProcessById(docElement.Current.ProcessId)) : string.Empty,
-                        IsCurrentTab = windowName.Contains(elementName)
-                    });
+                    var url = (docElement.GetCurrentPattern(pattern) as ValuePattern).Current.Value.ToString();
+                    return url;
                 }
             }
-            return result;
-        }
-
-        private string FindWindowUrl(Process process)
-        {
-            AutomationElement element = AutomationElement.FromHandle(process.MainWindowHandle);
-            if (element != null)
-            {
-                Condition conditions = new AndCondition(
-                    new PropertyCondition(AutomationElement.ProcessIdProperty, process.Id),
-                    new PropertyCondition(AutomationElement.IsControlElementProperty, true),
-                    new PropertyCondition(AutomationElement.IsContentElementProperty, true),
-                    new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit));
-
-                AutomationElement elementx = element.FindFirst(TreeScope.Descendants, conditions);
-                var res = ((ValuePattern)elementx.GetCurrentPattern(ValuePattern.Pattern)).Current.Value as string;
-                return res;
-            }
-
             return string.Empty;
         }
     }
